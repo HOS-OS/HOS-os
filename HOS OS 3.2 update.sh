56d351fce14882e1d4168d5df3ba90f53faf64a2
HOS-OS <86213274+HOS-OS@users.noreply.github.com>
2021-06-22 21:07:20 -0400

    Update HOS OS 3.2 update.sh

--- HOS OS 3.2 update.sh
+++ HOS OS 3.2 update.sh
@@ -5,7 +5,7 @@ select yn in "Yes" "No"; do
     case $yn in
 Yes ) sudo apt update ; sudo apt install -y tlp ; sudo apt-get install - y blueman; sudo apt-get install -y lxrandr ; sed -i 's/DISTRIB_DESCRIPTION="Hos OS 3.1"/DISTRIB_DESCRIPTION="Hos OS 3.2"/' /etc/lsb-release ; sed -i 's/NAME="Ubuntu"/NAME="HOS OS"/' /usr/lib/os-release  ; sed -i 's/PRETTY_NAME="HOS OS BETA  Live CD"/PRETTY_NAME="Hos OS 3.2"/' /usr/lib/os-release ; 
 
- echo "-> finished. your computer will reboot in 5 min seconds"; secs=$((100* 60))
+ echo "-> finished. your computer will reboot in 10 seconds"; secs=$((1* 10))
 while [ $secs -gt 0 ]; do
    echo -ne "$secs\033[0K\r"
    sleep 1
